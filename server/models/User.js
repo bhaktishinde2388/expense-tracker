3543bc6184d2dcc898beed81f8030afb91c022cc
bhakti <bhaktishinde2388@gmail.com>
2024-07-27 14:35:22 +0530

    creating model and contrller in server.creating schema for the user model

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+import { Schema , model } from "mongoose";
+
+const userSchema = new Schema({
+    name:{
+        typs:String,
+        required:true
+    },
+    email:{
+        typs:String,
+        required:true,
+        uniq:true
+    },
+    password:{
+        typs:String,
+        required:true
+    },
+    dob:{
+        typs:Date,
+        required:true
+    }
+});
+
+const User = model("User",userSchema);
+
+export default User;
