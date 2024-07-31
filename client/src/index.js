e77d16ac095f5705fd68eb03b7f1ae7a0a488fb4
bhakti <bhaktishinde2388@gmail.com>
2024-08-01 00:23:54 +0530

    created signp page and it pass data successfully to the database

---
+++ client/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from "react-dom/client"
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import Home from "./views/Home/Home.js"
+import Login from "./views/Login/Login.js"
+import SignUp from './views/SignUp/SignUp.js'
+import "./global.css"
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+const router = createBrowserRouter([
+    {
+    path:'/',
+    element:<Home />
+},
+{
+    path:"/login",
+    element:<Login />
+},
+{
+    path:"/signup",
+    element:<SignUp/>
+},
+{
+    path:"*",
+    element:<h1 >Ohh....
+        Page Not Found😑</h1>
+}
+
+])
+
+root.render(<RouterProvider router={router}/>)
+
