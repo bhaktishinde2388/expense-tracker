e74c3127bab96dd3a7b8ad2edc4b7f8352cb25c3
bhakti <bhaktishinde2388@gmail.com>
2024-08-05 01:08:51 +0530

    applied css on transaction card

--- client/src/components/TransactionCard/TransactionCard.js
+++ client/src/components/TransactionCard/TransactionCard.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
+import "./TransactionCard.css"
 function TransactionCard({ _id , title, amount, category, type,createdAt}) {
   return (
-    <>
-    <span> {title}</span>
-    </>
+    
+  <div className='transaction-card-container'>
+    <span className='title'> {title}</span>
+<span className='category'>{category}</span>
+<span className='type'>{type}</span>
+    <span className='amount'>{amount}</span>
+    <soan className="createdAt">{createdAt}</soan>
+    </div>
   )
 }
 
