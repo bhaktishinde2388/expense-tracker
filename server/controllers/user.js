d110c4da9780d13683a52733cf8da579c7d09937
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-11-01 18:21:36 +0530

    add validation for login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -80,6 +80,13 @@ const login =async (req,res)=>{
   
     const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "email and password are required",
+    });
+  }
+
     const user = await User.findOne({
       email: email,
       password: password
